53e1f46d52e7d57b9c690c654c9015c50750956a
Prus Anna <anna.prusm@fti.de>
2020-02-28 16:25:05 +0100

    added a condition

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -2,9 +2,7 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import Book from './Book';
 
-
 class SearchPage extends React.Component {
-
 
   render() {
     const { onSearch, onChange, books} = this.props;
@@ -28,8 +26,10 @@ class SearchPage extends React.Component {
      
           <ol className="books-grid">
           {books && books.map((book, index) => {
+              if (book.shelf=== 'none'){
               return <Book key={book.id}  book={book} index={index} onChange={onChange} />;
-            })
+            } else{return console.log('this book is already in the shelves');}
+          })
           }
           </ol>
  
