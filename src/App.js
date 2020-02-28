175b11945837092903aa57284aa35a0901e61ba2
Prus Anna <anna.prusm@fti.de>
2020-02-28 15:59:00 +0100

    resolved the error in the console

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react'
-import * as BooksAPI from './BooksAPI'
-import './App.css'
-import {
-  BrowserRouter as Router,
-  Route
-} from "react-router-dom";
-import SearchPage from './SearchPage';
-import HomePage from './HomePage';
-
+import React from "react";
+import * as BooksAPI from "./BooksAPI";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import SearchPage from "./SearchPage";
+import HomePage from "./HomePage";
 class BooksApp extends React.Component {
   state = {
     books: [],
     booksSearched: [],
     query: ""
   };
-
-  setDefaultShelves = (searchedBooks, myBooks) => {
-
-    console.log('searchedBooks', searchedBooks)
-
+  setDefaultShelves = searchedBooks => {
+    const myBooks = this.state.books;
     return searchedBooks.map(book => {
       book.shelf = "none";
       myBooks.forEach(myBook => {
@@ -29,28 +22,27 @@ class BooksApp extends React.Component {
       return book;
     });
   };
-
-  updateQuery = (event) => { 
+  updateQuery = event => {
     const { value } = event.target;
-    console.log('query:', value);
-
-    BooksAPI.search(value).then(booksSearched => {
-      booksSearched && booksSearched.length 
-      ? this.setState({ booksSearched }) 
-      : this.setState({ booksSearched: [] });
-      
-      this.setDefaultShelves(booksSearched, this.props.books);
-    }).catch(error => console.log(error));
-  }
-
+    const { books } = this.state;
+    BooksAPI.search(value)
+      .then(booksSearched => {
+        if (booksSearched && booksSearched.length) {
+          const newBooks = this.setDefaultShelves(booksSearched);
+          this.setState({ booksSearched, books: [...books, ...newBooks] });
+        } else {
+          this.setState({ booksSearched: [] });
+        }
+      })
+      .catch(error => console.log(error));
+  };
   componentDidMount() {
     BooksAPI.getAll().then(books => {
       console.log("books: ", books);
-      this.setState({books});
+      this.setState({ books });
     });
   }
-
-  onSelfChange = (event, book) => { 
+  onSelfChange = (event, book) => {
     const { value } = event.target;
     const { books } = this.state;
     const index = books.indexOf(book);
@@ -59,9 +51,8 @@ class BooksApp extends React.Component {
     this.setState({
       books: modifiedBooks
     });
-    BooksAPI.update(book, value)
-  }
-
+    BooksAPI.update(book, value);
+  };
   render() {
     return (
       <Router>
@@ -69,13 +60,25 @@ class BooksApp extends React.Component {
           <Route
             exact
             path="/"
-            render={() => <HomePage books={this.state.books} onChange={this.onSelfChange} />}
+            render={() => (
+              <HomePage books={this.state.books} onChange={this.onSelfChange} />
+            )}
           />
-          <Route exact path="/search" render={() => <SearchPage books={this.state.booksSearched} onChange={this.updateQuery} onSearch={this.updateQuery} />} />
+          <Route
+            exact
+            path="/search"
+            render={() => (
+              <SearchPage
+                books={this.state.booksSearched}
+                onChange={this.onSelfChange}
+                onSearch={this.updateQuery}
+              />
+            )}
+          />
         </div>
       </Router>
     );
   }
 }
+export default BooksApp;
 
-export default BooksApp
